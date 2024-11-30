f9d51f3cee4033ee176070d8e2c7e1fc4e6d4ee9
Aqib Bhat <aqib.n.bhat@gmail.com>
2024-11-30 11:19:07 -0800

    Set format for output

--- popup.js
+++ popup.js
@@ -3,15 +3,15 @@ document.getElementById("structureIt").addEventListener("click", async () => {
   const outputStructured = document.getElementById("outputStructured");
 
   outputStructured.textContent = "Getting results ...";
-  rewriter = await ai.rewriter.create({
+  const rewriterApi = await ai.rewriter.create({
+    tone: "as-is",
+    format: "markdown",
     sharedContext:
       "I have some thoughts on my mind that I want to share in an email or chat." +
       "For each text I submit, rewrite it as bullet points with the following sections: " +
       "Background, Updates, Questions, Action Items, TLDR",
   });
-  let response = await rewriter.rewrite(reviewEl.textContent, {
-    context: inputThoughtsToPost,
-  });
+  let response = await rewriterApi.rewrite(inputThoughtsToPost);
 
   outputStructured.textContent = response;
 });
