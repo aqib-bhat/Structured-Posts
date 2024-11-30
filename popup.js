460ff29fc3d517a06adfdcb108d2496254113372
Aqib Bhat <aqib.n.bhat@gmail.com>
2024-11-30 11:44:03 -0800

    Only tl;dr

--- popup.js
+++ popup.js
@@ -4,9 +4,8 @@ document.getElementById("structureIt").addEventListener("click", async () => {
 
   outputStructured.textContent = "Getting results ...";
   const rewriterApi = await ai.rewriter.create({
-    sharedContext:
-      "Rewrite the given text with the following sections: " +
-      "Background, Updates, Questions, Action Items, tl;dr",
+    sharedContext: "Rewrite the given text in one sentence like a tl;dr",
+    // "Background, Updates, Questions, Action Items, tl;dr",
   });
   let response = await rewriterApi.rewrite(inputThoughtsToPost);
 
