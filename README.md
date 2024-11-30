3715beb7dc87442fb2bc037a265c902fe5188007
Aqib Bhat <aqib.n.bhat@gmail.com>
2024-11-30 12:04:06 -0800

    Switch to Summarizer API

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Structured-Posts
 
 - A browser extension using Chrome's built-in AI for organizing text in a concise manner with a structure and bullet points.
-- [Built-in AI APIs](https://developer.chrome.com/docs/ai/built-in-apis) used: Rewriter API
+- [Built-in AI APIs](https://developer.chrome.com/docs/ai/built-in-apis) used: Summarizer API
 - Related video: [Watch Structured-Posts on YouTube](https://www.youtube.com/watch?v=W)
--- popup.js
+++ popup.js
@@ -3,11 +3,15 @@ document.getElementById("structureIt").addEventListener("click", async () => {
   const outputStructured = document.getElementById("outputStructured");
 
   outputStructured.textContent = "Getting results ...";
-  const rewriterApi = await ai.rewriter.create({
-    sharedContext: "Rewrite the given text in one sentence like a tl;dr",
-    // "Background, Updates, Questions, Action Items, tl;dr",
+  const summarizerAPI = await ai.summarize.create({
+    type: "text",
+    length: "shorter",
+    format: "markdown",
+    sharedContext:
+      "Summarize the given text with the following sections: " +
+      "'Background', 'Updates', 'Questions', 'Action Items', 'tl;dr'",
   });
-  let response = await rewriterApi.rewrite(inputThoughtsToPost);
+  let response = await summarizerAPI.summarize(inputThoughtsToPost);
 
   outputStructured.textContent = response;
 });
